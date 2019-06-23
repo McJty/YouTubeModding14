4f349aa8bc293c90ca08c9ab2d44c6de5cc7ecf5
McJty <mcjty1@gmail.com>
2019-06-23 05:15:57 +0200

    Better item handler handling

--- src/main/java/com/mcjty/mytutorial/blocks/FirstBlockTile.java
+++ src/main/java/com/mcjty/mytutorial/blocks/FirstBlockTile.java
@@ -3,12 +3,15 @@ package com.mcjty.mytutorial.blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.INBT;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.INBTSerializable;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 
 import javax.annotation.Nonnull;
@@ -18,7 +21,8 @@ import static com.mcjty.mytutorial.blocks.ModBlocks.FIRSTBLOCK_TILE;
 
 public class FirstBlockTile extends TileEntity implements ITickableTileEntity {
 
-    private ItemStackHandler handler;
+    private LazyOptional<IItemHandler> handler = LazyOptional.of(this::createHandler);
+
 
     public FirstBlockTile() {
         super(FIRSTBLOCK_TILE);
@@ -31,43 +35,42 @@ public class FirstBlockTile extends TileEntity implements ITickableTileEntity {
     @Override
     public void read(CompoundNBT tag) {
         CompoundNBT invTag = tag.getCompound("inv");
-        getHandler().deserializeNBT(invTag);
+        handler.ifPresent(h -> ((INBTSerializable<CompoundNBT>)h).deserializeNBT(invTag));
         super.read(tag);
     }
 
     @Override
     public CompoundNBT write(CompoundNBT tag) {
-        CompoundNBT compound = getHandler().serializeNBT();
-        tag.put("inv", compound);
+        handler.ifPresent(h -> {
+            CompoundNBT compound = ((INBTSerializable<CompoundNBT>)h).serializeNBT();
+            tag.put("inv", compound);
+        });
         return super.write(tag);
     }
 
-    private ItemStackHandler getHandler() {
-        if (handler == null) {
-            handler = new ItemStackHandler(1) {
-                @Override
-                public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
-                    return stack.getItem() == Items.DIAMOND;
-                }
+    private IItemHandler createHandler() {
+        return new ItemStackHandler(1) {
+            @Override
+            public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
+                return stack.getItem() == Items.DIAMOND;
+            }
 
-                @Nonnull
-                @Override
-                public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
-                    if (stack.getItem() != Items.DIAMOND) {
-                        return stack;
-                    }
-                    return super.insertItem(slot, stack, simulate);
+            @Nonnull
+            @Override
+            public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
+                if (stack.getItem() != Items.DIAMOND) {
+                    return stack;
                 }
-            };
-        }
-        return handler;
+                return super.insertItem(slot, stack, simulate);
+            }
+        };
     }
 
     @Nonnull
     @Override
     public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
         if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
-            return LazyOptional.of(() -> (T) getHandler());
+            return handler.cast();
         }
         return super.getCapability(cap, side);
     }

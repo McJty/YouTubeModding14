d2b0d97f6319ae6038658abf038639a1a198c38d
McJty <mcjty1@gmail.com>
2019-09-15 05:39:36 +0200

    Data generators!

---
+++ src/main/java/com/mcjty/mytutorial/datagen/LootTables.java
@@ -0,0 +1,16 @@
+package com.mcjty.mytutorial.datagen;
+
+import com.mcjty.mytutorial.blocks.ModBlocks;
+import net.minecraft.data.DataGenerator;
+
+public class LootTables extends BaseLootTableProvider {
+
+    public LootTables(DataGenerator dataGeneratorIn) {
+        super(dataGeneratorIn);
+    }
+
+    @Override
+    protected void addTables() {
+        lootTables.put(ModBlocks.FIRSTBLOCK, createStandardTable("firstblock", ModBlocks.FIRSTBLOCK));
+    }
+}
---
+++ src/main/java/com/mcjty/mytutorial/datagen/Recipes.java
@@ -0,0 +1,32 @@
+package com.mcjty.mytutorial.datagen;
+
+import com.mcjty.mytutorial.blocks.ModBlocks;
+import net.minecraft.advancements.criterion.InventoryChangeTrigger;
+import net.minecraft.block.Blocks;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.IFinishedRecipe;
+import net.minecraft.data.RecipeProvider;
+import net.minecraft.data.ShapedRecipeBuilder;
+import net.minecraftforge.common.Tags;
+
+import java.util.function.Consumer;
+
+public class Recipes extends RecipeProvider {
+
+    public Recipes(DataGenerator generatorIn) {
+        super(generatorIn);
+    }
+
+    @Override
+    protected void registerRecipes(Consumer<IFinishedRecipe> consumer) {
+        ShapedRecipeBuilder.shapedRecipe(ModBlocks.FIRSTBLOCK)
+                .patternLine("xxx")
+                .patternLine("x#x")
+                .patternLine("xxx")
+                .key('x', Blocks.COBBLESTONE)
+                .key('#', Tags.Items.DYES_RED)
+                .setGroup("mytutorial")
+                .addCriterion("cobblestone", InventoryChangeTrigger.Instance.forItems(Blocks.COBBLESTONE))
+                .build(consumer);
+    }
+}
